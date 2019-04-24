ba29f2195d26e141e8452f5cfa7d147932eec04e
Alejandra Izaguirre <aleizatebar@gmail.com>
2019-04-24 18:50:43 +0200

    new file:   Codigo/Renting/bin/.gitignore 	new file:   Codigo/Renting/src/modelos/Administrador.java

---
+++ Codigo/Renting/src/modelos/Administrador.java
@@ -0,0 +1,36 @@
+package modelos;
+
+public class Administrador {
+	private String usuario;
+	private String contrasenia;
+	
+	public Administrador(String usuario, String contrasenia) {		
+		this.usuario = usuario;
+		this.contrasenia = contrasenia;
+	}
+	
+	public Administrador () {}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [usuario=" + usuario + ", contrasenia=" + contrasenia + "]";
+	}
+	
+	
+}
