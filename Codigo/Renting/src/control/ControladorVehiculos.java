ec763a1bf08e441a0e94c9095fcffdfd8bcece78
alejandro <moflycy@gmail.com>
2019-05-16 09:49:14 +0200

    controlador vehiculos

---
+++ Codigo/Renting/src/control/ControladorVehiculos.java
@@ -0,0 +1,60 @@
+package control;
+
+import java.util.List;
+
+import javax.swing.plaf.PanelUI;
+
+import dao.VehiculosDAO;
+import interfaces.InterfazVehiculos;
+import interfaces.InterfazVehiculos.Vista;
+import modelos.Vehiculos;
+
+public class ControladorVehiculos implements InterfazVehiculos.Controlador {
+	private InterfazVehiculos.Vista panelVehiculo;
+	private VehiculosDAO vehiculosDAO;
+	public ControladorVehiculos(Vista panelVehiculo) {
+		this.panelVehiculo = panelVehiculo;
+		this.vehiculosDAO = new VehiculosDAO();
+	}
+	@Override
+	public void obtenerVehiculossAlta() {
+		List<Vehiculos> listaVehiculos = vehiculosDAO.obtenerVehiculosActivos();
+		panelVehiculo.mostrarVehiculos(listaVehiculos);
+	}
+	@Override
+	public void obtenerVehiculosBaja() {
+		// TODO Auto-generated method stub
+		List<Vehiculos> listaVehiculos = vehiculosDAO.obtenerVehiculosBaja();
+		panelVehiculo.mostrarVehiculossHistorial(listaVehiculos);
+	}
+	
+	@Override
+	public void obtenerVehiculosTotal() {
+		// TODO Auto-generated method stub
+		List<Vehiculos> listaVehiculos = vehiculosDAO.obtenerTodosLosVehiculos();
+		panelVehiculo.mostrarVehiculos(listaVehiculos);
+	}
+	@Override
+	public void registrarVehiculos(Vehiculos vehiculoRegistrar) {
+		if(vehiculoRegistrar != null){
+			if(!vehiculosDAO.insertarVehiculos(vehiculoRegistrar)) {
+				panelVehiculo.errorEnRegistroDeVehiculos();
+			}else {
+				panelVehiculo.insercionCorrecta();
+				obtenerVehiculossAlta();
+			}
+				
+		}
+	}
+	@Override
+	public void editarVehiculos(String matricula, Vehiculos vehiculoAEditar) {
+		// TODO Auto-generated method stub
+		if(vehiculoAEditar != null) {
+			//vehiculosDAO.actualizarEstadoYKmvehiculo(matricula, estado)
+			obtenerVehiculossAlta();
+		}
+	}
+	
+	
+
+}
