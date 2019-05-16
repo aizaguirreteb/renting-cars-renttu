2cabe463768135dff04ebf5d31edf6b94a21ad6c
alejandro <moflycy@gmail.com>
2019-05-16 10:28:52 +0200

    dialogo vehiculos lo terminamos mañana

---
+++ Codigo/Renting/src/dialogos/DialogoFormularioVehiculo.java
@@ -0,0 +1,15 @@
+package dialogos;
+
+import javax.swing.JDialog;
+
+import modelos.Reserva;
+import modelos.Vehiculos;
+
+public class DialogoFormularioVehiculo extends JDialog {
+
+	private Vehiculos vehiculo = null;
+	
+	public Vehiculos getVehiculos() {
+		return vehiculo;
+	}
+}
