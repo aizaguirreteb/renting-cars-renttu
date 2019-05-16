7e19cd1a6b4876b1a29e650f63c7bcd385606d6f
Rubén Núñez Valle <rubennuvalle@gmail.com>
2019-05-16 12:22:25 +0200

    PanelClientes

---
+++ Codigo/Renting/src/vistas/PanelClientes.java
@@ -0,0 +1,215 @@
+package vistas;
+
+import interfaces.InterfazCliente;
+import modelos.Cliente;
+import control.ControladorClientes;
+import modelosTablas.ModeloTablaClientes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import auxiliar.Auxiliar;
+
+public class PanelClientes extends JPanel implements InterfazCliente.Vista {
+	private JTable tableReservas;
+	private JTextField textBusqueda;
+	private JScrollPane scrollPane;
+	private JSeparator separator;
+	private JButton btnNuevaReserva;
+	private JButton btnHistorial;
+	private JButton btnBuscar;
+	private JSeparator separator_1;
+	private GroupLayout groupLayout;
+
+	private InterfazCliente.Controlador controladorC;
+
+	public PanelClientes() {
+
+		controladorC = new ControladorClientes(this);
+		setPanel();
+		controladorC.obtenerListaClientesAlta();
+		btnNuevaReserva.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				nuevoCliente();
+			}		
+		});
+
+		btnHistorial.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controladorC.obtenerListaClientesBaja();
+			}		
+		});
+
+	}
+
+	private void setPanel() {
+		scrollPane = new JScrollPane();
+		setTabla();
+		scrollPane.setViewportView(tableReservas);
+		crearSeparadores();		
+		crearBotones();		
+		crearTextFields();		
+		insertarObjetos();		
+		setLayout(groupLayout);
+	}
+	private void setTabla() {
+		tableReservas = new JTable();
+		tableReservas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tableReservas.setRowSelectionAllowed(true);
+		tableReservas.setColumnSelectionAllowed(false);
+		tableReservas.setCellSelectionEnabled(false);
+		tableReservas.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if (e.getValueIsAdjusting()) return;
+				ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+				if (lsm.isSelectionEmpty()) {
+					System.out.println("No hay selecci�n.");
+				} else {
+					int selectedRow = lsm.getMinSelectionIndex();
+					editarCliente(selectedRow);
+				}	
+			}
+		});
+	}
+	private void insertarObjetos() {
+		groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addContainerGap()
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE)
+								.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createSequentialGroup()
+														.addComponent(btnNuevaReserva, GroupLayout.PREFERRED_SIZE, 70, Short.MAX_VALUE)
+														.addGap(225)
+														.addComponent(btnHistorial, GroupLayout.PREFERRED_SIZE, 44, Short.MAX_VALUE))
+												.addComponent(separator, GroupLayout.DEFAULT_SIZE, 425, Short.MAX_VALUE))
+										.addGap(1))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(textBusqueda, GroupLayout.DEFAULT_SIZE, 168, Short.MAX_VALUE)
+										.addGap(18)
+										.addComponent(btnBuscar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addGap(167))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(separator_1, GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE)
+										.addGap(4)))
+						.addGap(14))
+				);
+		groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addContainerGap()
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 148, GroupLayout.PREFERRED_SIZE)
+						.addGap(18)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 4, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textBusqueda, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnBuscar))
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 4, GroupLayout.PREFERRED_SIZE)
+						.addGap(10)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNuevaReserva)
+								.addComponent(btnHistorial))
+						.addContainerGap(37, Short.MAX_VALUE))
+				);
+	}
+
+	private void crearTextFields() {
+		textBusqueda = new JTextField();
+		textBusqueda.setColumns(10);
+	}
+
+	private void crearSeparadores() {
+		separator = new JSeparator();
+		separator_1 = new JSeparator();
+	}
+
+	private void crearBotones() {
+		btnNuevaReserva = new JButton("NUEVA RESERVA");
+		
+		btnHistorial = new JButton("HISTORIAL");
+		btnBuscar = new JButton("BUSCAR");
+	}
+	
+	private void nuevoCliente() {
+		DialogoFormularioClientes dialogoClientes = new DialogoFormularioClientes(PanelClientes.this);
+		Cliente cliente = dialogoClientes.getCliente();
+		controladorC.crearNuevoCliente(cliente);
+		
+	}
+	
+	public void editarCliente(int filaSeleccionada) {
+		ModeloTablaClientes modeloTabla = (ModeloTablaClientes) tableReservas.getModel();
+		Cliente clienteAEditar = modeloTabla.getListaCliente().get(filaSeleccionada);
+		DialogoFormularioCliente dialogo = new DialogoFormularioCliente(PanelClientes.this);
+		Cliente nuevoCliente = dialogo.getCliente();
+		dialogo.getBotonDarDeBaja().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(nuevoCliente!= null)
+					controladorC.actualizarCliente(clienteAEditar.getDni(),nuevoCliente);
+				
+			}
+		}
+		);
+		
+		
+	}
+
+	@Override
+	public void mostrarClientes(List<Cliente> listaClientes) {
+		tableReservas.setModel(new ModeloTablaClientes(listaClientes));		
+		
+	}
+
+
+
+	@Override
+	public void mostrarClientesBaja(List<Cliente> listaClientesBaja) {
+		tableReservas.setModel(new ModeloTablaClientes(listaClientesBaja));			
+		
+	}
+
+	@Override
+	public void errorAlRegistrarCliente() {
+		JOptionPane.showMessageDialog(this, "No es posible registrar el cliente.");
+		
+	}
+
+	@Override
+	public void registroCorrecto() {
+		JOptionPane.showMessageDialog(this, "Nuevo cliente Registrado");
+		
+	}
+
+	@Override
+	public void clienteDadoDeBajaCorrectamente() {
+		JOptionPane.showMessageDialog(this, "Cliente dado de baja ");
+		
+	}
+	
+}
